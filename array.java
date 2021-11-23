0b570f7b5cb234d9cef7d3effb66c9475eef0ee2
Gabriel Morales Martínez <gabrielmorales1996@gmail.com>
2021-11-23 22:31:13 +0100

    He añadido el array

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array{
+
+    public static void main(String[] args) {
+        int[] array=new int[20];
+
+        for (int i=0; i < array.length; i++)
+        {
+            array[i]=(int)((Math.random()*10)+1);
+        }
+    }
+}
