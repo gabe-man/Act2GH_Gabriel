7f88e636257a6189c35d39d2028dc5c15947f871
Gabriel Morales Martínez <gabrielmorales1996@gmail.com>
2021-11-23 22:44:31 +0100

    Ahora se escribiran 30 numeros en el array

--- array.java
+++ array.java
@@ -1,7 +1,7 @@
 public class array{
 
     public static void main(String[] args) {
-        int[] array=new int[20];
+        int[] array=new int[30];
 
         for (int i=0; i < array.length; i++)
         {
