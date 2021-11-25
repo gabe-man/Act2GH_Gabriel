605f608838f589d8928cb6329fe86838658b45f8
adrian <a_adrian.camara.munoz@iespablopicasso.es>
2021-11-25 14:00:06 +0100

    mejoras a gabi

--- array.java
+++ array.java
@@ -27,5 +27,55 @@ public class array{
             System.out.println(i + " aparece " + cont + " veces");
             cont=0;
         }
+
+        System.out.println("Esta fatal, arreglalo anda.");
+        System.out.println("Seria mejor asi: ");
+
+        for (int i=0; i < array.length; i++)
+        {
+            array[i]=(int)((Math.random()*10)+1);
+        }
+
+        for (int i=0; i<array.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < array.length; j++) 
+            {
+                if (array[j]==i)
+                {
+                    cont++;
+                }
+            }
+            System.out.println(i + " aparece " + cont + " veces");
+            cont=0;
+        }
+
+         System.out.println("Esta mal, te recomiendo hacer lo siguiente: ");
+         
+         for (int i=0; i < array.length; i++)
+        {
+            array[i]=(int)((Math.random()*10)+1);
+        }
+
+        for (int i=0; i<array.length; i++)
+        {
+            System.out.println(array[i]);
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < array.length; j++) 
+            {
+                if (array[j]==i)
+                {
+                    cont++;
+                }
+            }
+            System.out.println(i + " aparece " + cont + " veces");
+            cont=0;
+        }
+
     }
 }
